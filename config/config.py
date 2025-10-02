931c727201951dd6ed1da53e700929bb6473527b
xyzHooDlEzyx <93081557+xyzHooDlEzyx@users.noreply.github.com>
2025-10-02 12:26:12 +0300

    Refactor configuration management by removing YAML file and implementing AWS Secrets Manager for sensitive data retrieval

---
+++ config/config.py
@@ -0,0 +1,43 @@
+import boto3
+import json
+import os
+
+
+def get_secret(secret_name, region_name="eu-north-1"):
+    """Отримати секрет з AWS Secrets Manager"""
+    client = boto3.client("secretsmanager", region_name=region_name)
+
+    response = client.get_secret_value(SecretId=secret_name)
+
+    if "SecretString" in response:
+        secret = response["SecretString"]
+        return json.loads(secret)  # повертає dict
+    else:
+        return json.loads(response["SecretBinary"])
+
+
+class Config:
+    DEBUG = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret")
+
+    try:
+        secret = get_secret("prod/mysql/beta")  # твоя назва секрету в AWS
+
+        DB_USER = secret["username"]
+        DB_PASSWORD = secret["password"]
+        DB_HOST = secret["host"]
+        DB_PORT = secret["port"]
+        DB_NAME = os.getenv("DB_NAME", "private_bank_lab5")  # <-- задаєш вручну, якщо нема в секреті
+
+        SQLALCHEMY_DATABASE_URI = (
+            f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        )
+    except Exception as e:
+        print("⚠️ Не вдалося отримати секрет з AWS:", e)
+        # fallback для локального тесту
+        SQLALCHEMY_DATABASE_URI = os.getenv(
+            "DATABASE_URL",
+            "mysql+pymysql://root:password@localhost:3306/private_bank_lab5"
+        )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
